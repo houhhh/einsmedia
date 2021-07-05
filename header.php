442a308678fb207d214748b6c31bdfdb60c106ef
bostokc <houhh1@gmail.com>
2021-07-06 00:01:06 +0900

    working on header.php

---
+++ header.php
@@ -0,0 +1,44 @@
+<header class="mainTop">
+    
+    <h1 class="logo">
+        <a href="index.php">
+            <span class="blind">einsmedia</span>
+        </a>
+    </h1>
+    <nav class="mainNav">
+        <ul>
+            <li>
+                <a href="#">Business</a>
+            </li>
+            <li>
+                <a href="#">Why EINS</a>
+            </li>
+            <li>
+                <a href="#">Solution</a>
+            </li>
+            <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="#">Company</a>
+            </li>
+            <li>
+                <a href="#">People</a>
+            </li>
+            
+        </ul>
+        <div class="btnHam">
+            <a href="#"><span class="lines"></span></a>
+        </div>
+    </nav>
+</header>
+<section class="mainSlide">
+    <div class="mainTitle">
+        <h2>
+            More than advertising<br>
+            expert 'einsmedia'
+        </h2>
+        <h3>사용자와 클라이언트를 연결해 주는 No1디지털 마케팅 그룹</h3>
+    </div>
+    
+</section>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include './fixheader.php'; ?>
+<title>einsmedia</title>
+</header>
+<body>
+    <?php include "./header.php"; ?>
+    
+
+
+</body>
