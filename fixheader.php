c0886f6864ed1166a0e9e4177978ace917bcf975
bostokc <houhh1@gmail.com>
2021-07-11 02:08:38 +0900

    fixed ham and disappeared header manu bar

--- fixheader.php
+++ fixheader.php
@@ -6,10 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" href="./css/common.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;600;900&display=swap" rel="stylesheet">
     <script src="http://code.jquery.com/jquery-1.11.2.js"></script>
     <script src="https://kit.fontawesome.com/9b4d07ff5a.js" crossorigin="anonymous"></script>
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;600;900&display=swap');
+        body {
+            font-family: 'Montserrat', sans-serif;
+        }
     </style>
    
     
